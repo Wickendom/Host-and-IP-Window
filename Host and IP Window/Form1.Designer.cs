dd9c13b3c3dacbdfe8eedcbdef9d5a3203d0bba9
Wickendom <lwick1994@hotmail.com>
2023-02-23 14:18:47 +0000

    Add project files.

---
+++ Host and IP Window/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Host_and_IP_Window
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hostNameValue = new System.Windows.Forms.Label();
+            this.IPValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address:";
+            // 
+            // hostNameValue
+            // 
+            this.hostNameValue.AutoSize = true;
+            this.hostNameValue.Location = new System.Drawing.Point(88, 9);
+            this.hostNameValue.Name = "hostNameValue";
+            this.hostNameValue.Size = new System.Drawing.Size(38, 15);
+            this.hostNameValue.TabIndex = 2;
+            this.hostNameValue.Text = "label3";
+            // 
+            // IPValue
+            // 
+            this.IPValue.AutoSize = true;
+            this.IPValue.Location = new System.Drawing.Point(88, 24);
+            this.IPValue.Name = "IPValue";
+            this.IPValue.Size = new System.Drawing.Size(38, 15);
+            this.IPValue.TabIndex = 3;
+            this.IPValue.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(196, 54);
+            this.Controls.Add(this.IPValue);
+            this.Controls.Add(this.hostNameValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label hostNameValue;
+        private Label IPValue;
+    }
+}
---
+++ Host and IP Window/Form1.cs
@@ -0,0 +1,40 @@
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace Host_and_IP_Window
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            //sets the label value for the host name to the local machines name
+            hostNameValue.Text = System.Environment.GetEnvironmentVariable("COMPUTERNAME");
+            IPValue.Text = GetIP();
+            TopMost = true; // set the window to always be on top
+            MoveWindow();
+        }
+
+        //Connect to a UDP port to get the required IP address
+        //Connecting to UDP means we can get an accurate IP if you are using multiple network adapters
+        private string GetIP()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            return endPoint.Address.ToString(); ;
+        }
+
+        //set the window to be in the top right corner slightly below the top of the screen
+        private void MoveWindow()
+        {
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Right - this.Width, 35);
+        }
+    }
+
+    
+}
